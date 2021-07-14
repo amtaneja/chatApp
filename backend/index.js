71426562b105a2424a7e6b9271062de777193013
astar enterprises <astarenterprises48@gmail.com>
2021-07-14 12:04:43 +0530

    first commit

---
+++ backend/index.js
@@ -0,0 +1,46 @@
+//node server which will handle socket io connection
+
+const io = require('socket.io')(process.env.port || 5000, {
+    cors: {
+        origin: '*'
+    }
+})
+
+const users = {}
+
+//predefined on connection it will listen all socket connection like aman ram krishna 
+//different user are listen through io.on
+
+io.on('connection', socket => {
+
+    //user defined on , (new user joined event)
+    //instance of server
+    //it will listen the only when some person is joined , user jab join hoga toh kya karna hai
+    //that will put in callback function
+    socket.on('new-user-joined', name => {
+
+        console.log('new user : ', name)
+
+        users[socket.id] = name
+
+        //jab koi new user join hoga toh sabko pata chalna chaiye
+        //socket.broadcast.emit event bhejta hai sabko except the person who joined
+        socket.broadcast.emit('user-joined', name)
+
+    })
+
+
+    //chat message bhejne ka event
+    //jaise send event call hoga baki logo ko message aur name broadcast ho jyaga
+    socket.on('send', message => {
+        socket.broadcast.emit('receive', { message: message, name: users[socket.id] })
+    })
+
+
+    socket.on('disconnect', message => {
+        socket.broadcast.emit('leave', users[socket.id])
+        delete users[socket.id]
+    })
+
+
+})
---
+++ frontend/client.js
@@ -0,0 +1,47 @@
+const socket = io('http://localhost:5000')
+
+
+const form = document.getElementById('send-container')
+const messageInput = document.getElementById('messageInp')
+const messageContainer = document.querySelector('.container')
+const audio = new Audio('sound.mp3')
+
+
+const append = (message, position) => {
+    const messageElement = document.createElement('div')
+    messageElement.innerText = message;
+    messageElement.classList.add('message')
+    messageElement.classList.add(position)
+    messageContainer.append(messageElement)
+
+    if (position == 'left') {
+        audio.play()
+    }
+}
+
+form.addEventListener('submit', (e) => {
+    e.preventDefault();
+    const message = messageInput.value
+    append(`You : ${message}`, 'right')
+    socket.emit('send', message)
+    messageInput.value = ''
+})
+
+
+let userName = prompt('enter your name to join ...')
+
+socket.emit('new-user-joined', userName)
+
+socket.on('user-joined', name => {
+
+    append(`${name} joined the chat `, 'center')
+})
+
+socket.on('receive', data => {
+
+    append(`${data.name} : ${data.message}`, 'left')
+})
+
+socket.on('leave', data => {
+    append(`${data} left the chat.`, 'center')
+})
